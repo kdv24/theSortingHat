a2f7762b2be38560ec4d0d8f320beb6b976013bd
Bojana Skarich & erica cha <student@epicodus.com>
2015-05-06 15:35:42 -0700

    Add images and jquery, stylesheet.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function() {
+    $([IMAGEMAPPING]).submit(function(event) {
+        var firstimage = $("img1").val());
+        var secondimage = $("img2").val());
+        var thirdimage = $("img3").val());
+        var fourthimage = $("img4").val());
+        var fifthimage = $("img5").val());
+    })
+});
