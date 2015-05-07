94347be0da68e631f86d19cd9494226977a49a07
erica cha & Bojana Skarich & Abeer Khakwani & Kelly de Vries <student@epicodus.com>
2015-05-07 14:05:24 -0700

    image will disappear after 5 selected.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
+
 $(document).ready(function() {
 
 var clickCount = 0;
 
 var Houses = {
-    gryffindor: 0,
-    ravenclaw: 0,
-    hufflepuff: 0,
-    slytherin: 0,
+    'gryffindor':0,
+    'ravenclaw': 0,
+    'hufflepuff': 0,
+    'slytherin': 0,
+}
+var hideimages= function(){
+    if(clickCount >= 5){
+    $("form#image").hide();
+    }
 }
 
 
-//click makes the following happen-
 
     $("img.g").click(function(){
         alert("gryffindor");
@@ -18,6 +23,8 @@ var Houses = {
         clickCount += 1;
         $(this).hide();
         console.log(Houses.gryffindor);
+        hideimages();
+
     });
     $("img.s").click(function(){
         alert("slytherin ");
@@ -25,13 +32,17 @@ var Houses = {
         clickCount += 1;
         $(this).hide();
         console.log(Houses.slytherin);
+        hideimages();
+
     });
+
     $("img.h").click(function(){
         alert("hi hufflepuff");
         Houses.hufflepuff += 1;
         clickCount += 1;
         $(this).hide();
         console.log(Houses.hufflepuff);
+        hideimages();
     });
     $("img.r").click(function(){
         alert("hi ravenclaw");
@@ -39,8 +50,13 @@ var Houses = {
         clickCount += 1;
         $(this).hide();
         console.log(Houses.ravenclaw);
-    });
+        hideimages();
 
-    return Houses;
+
+    });
+    console.log("clickcounter" + clickCount);
+
+
+
 
 });
