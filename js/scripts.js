0a6ad41c97b42abd3a04df9271912ce64128877c
erica cha & Bojana Skarich & Abeer Khakwani & Kelly de Vries <student@epicodus.com>
2015-05-07 15:36:37 -0700

    add images and css rotation

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,74 @@
-
 $(document).ready(function() {
 
-var clickCount = 0;
+    var clickCount = 0;
 
-var Houses = {
-    'gryffindor':0,
-    'ravenclaw': 0,
-    'hufflepuff': 0,
-    'slytherin': 0,
-}
-var hideimages= function(){
-    if(clickCount >= 5){
-    $("form#image").hide();
+    var Houses = {
+        'gryffindor': 0,
+        'ravenclaw': 0,
+        'hufflepuff': 0,
+        'slytherin': 0,
     }
+    var hideimages = function() {
+        if ((clickCount >= 5) && ((Houses.gryffindor > 2) || (Houses.ravenclaw > 2) || (Houses.hufflepuff > 2) || (Houses.hufflepuff > 2) || (Houses.slytherin > 2))) {
+            $("form#image").hide();
+            $("h1").hide();
+            $("h3").hide();
+
+
+
+            if((Houses.gryffindor > Houses.ravenclaw)&&(Houses.gryffindor > Houses.hufflepuff) && (Houses.gryffindor > Houses.slytherin)) {
+                $(".result_personality").append("hello gryffindor");
+            } else if ((Houses.ravenclaw > Houses.gryffindor) && (Houses.ravenclaw > Houses.hufflepuff) && (Houses.ravenclaw > Houses.slytherin))  {
+                $(".result_personality").append("hello ravenclaw");
+            } else if ((Houses.hufflepuff> Houses.gryffindor) && (Houses.hufflepuff > Houses.ravenclaw) && (Houses.hufflepuff > Houses.slytherin))  {
+                $(".result_personality").append("hello hufflepuff");
+            } else if ((Houses.slytherin > Houses.gryffindor) && (Houses.slytherin > Houses.ravenclaw) && (Houses.slytherin > Houses.hufflepuff))  {
+                $(".result_personality").append("hello slytherin");
+
+
+            }else{ $(".result_personality").append("hello else");
 }
 
+            $("#result").show();
 
 
-    $("img.g").click(function(){
-        alert("gryffindor");
-        Houses.gryffindor += 1;
-        clickCount += 1;
+
+
+        }
+    }
+
+    var addHouse = function(house) {
+        Houses[house] += 1
+        clickCount += 1
+
+    }
+
+    $("img.g").click(function() {
         $(this).hide();
-        console.log(Houses.gryffindor);
-        hideimages();
-
-    });
-    $("img.s").click(function(){
-        alert("slytherin ");
-        Houses.slytherin += 1;
-        clickCount += 1;
-        $(this).hide();
-        console.log(Houses.slytherin);
-        hideimages();
-
-    });
-
-    $("img.h").click(function(){
-        alert("hi hufflepuff");
-        Houses.hufflepuff += 1;
-        clickCount += 1;
-        $(this).hide();
-        console.log(Houses.hufflepuff);
-        hideimages();
-    });
-    $("img.r").click(function(){
-        alert("hi ravenclaw");
-        Houses.ravenclaw += 1;
-        clickCount += 1;
-        $(this).hide();
-        console.log(Houses.ravenclaw);
+        addHouse('gryffindor');
         hideimages();
 
 
     });
-    console.log("clickcounter" + clickCount);
+    $("img.s").click(function() {
+        $(this).hide();
+        addHouse('slytherin');
+        hideimages();
+
+    });
+
+    $("img.h").click(function() {
+        $(this).hide();
+        addHouse('hufflepuff');
+        hideimages();
+    });
+    $("img.r").click(function() {
+        addHouse('ravenclaw');
+        $(this).hide();
+        hideimages();
+
+
+    });
 
 
 
